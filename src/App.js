4dd6a8c0aaaa9a18757cfe56ce710e5a3c527a25
Mickabtech <Mickabtech@gmail.com>
2023-07-28 12:18:58 +0100

    added the skill part

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from 'react'
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { NavBar } from './component/navbar'
-import { Banner } from './component/Banner'
+import { NavBar } from './component/navbar';
+import { Banner } from './component/Banner';
+import { Skills } from './component/Skills';
 
 const App = () => {
   return (
     <div className='App'>
       <NavBar />
       <Banner />
+      <Skills />
     </div>
   )
 }
